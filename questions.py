712757c86c300492daecb7b84546931b60c42326
clan <105570597+Clanart@users.noreply.github.com>
2026-07-27 16:41:54 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,5 +2,9 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "1inch/limit-order-protocol"
-REPO_NAME = "limit-order-protocol"
+SOURCE_REPO = "pushchain/push-chain-evm"
+REPO_NAME = "push-chain-evm"
+TREE = "96231e7"
+BRANCH = ""
+# Example:
+# https://github.com/pushchain/push-chain-evm/tree/96231e7
