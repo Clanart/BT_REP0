da5946089f26eae498a50c54885f27cb650441c8
clan <105570597+Clanart@users.noreply.github.com>
2026-08-10 20:18:43 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "celestiaorg/celestia-node"
-REPO_NAME = "celestia-node"
+SOURCE_REPO = "ExodusOSS/bytes"
+REPO_NAME = "bytes"
 TREE = ""
 BRANCH = ""
 # Example:
