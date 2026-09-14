27cbbab67b7685760822e06a7b26aa83fe90aa23
clan <105570597+Clanart@users.noreply.github.com>
2026-09-14 11:19:52 +0100

    Update questions.py

--- questions.py
+++ questions.py
@@ -2,8 +2,8 @@ import json
 import os
 
 MAX_REPO = 25
-SOURCE_REPO = "Chia-Network/chia-blockchain"
-REPO_NAME = "chia-blockchain"
+SOURCE_REPO = "kaiachain/kaia"
+REPO_NAME = "kaia"
 TREE = ""
 BRANCH = ""
 # Example:
